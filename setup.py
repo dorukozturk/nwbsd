c9402fac9fc9f8cf09f9c5b3749df245b6a68fcb
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-12-26 11:38:44 -0500

    Add minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nwbsd',
+    version='0.0.1',
+    packages=['nwbsd'],
+    license='Apache v2',
+    install_requires=[
+        "pynwb"
+    ]
+)
