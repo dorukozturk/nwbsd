c30d81701536a949d8842d4d8b47b6f6afdc72b9
Roni Choudhury <roni.choudhury@kitware.com>
2018-02-26 12:48:54 -0500

    Remove unnecessary `unleaf()` function

--- esv/data/dumptree.py
+++ esv/data/dumptree.py
@@ -14,27 +14,6 @@ def normalize(t):
         raise RuntimeError(type(t))
 
 
-def unleaf(tt):
-    def unleaf_helper(t):
-        if type(t) == str:
-            return t
-
-        keys = list(t.keys())
-        if len(keys) != 1:
-            raise RuntimeError('non-unique label')
-
-        key = keys[0]
-        val = t[key]
-
-        if type(val) == list and val and type(val[0]) == str:
-            return key
-        elif type(val) == list and val and type(val[0]) == dict:
-            return {key: list(map(unleaf_helper, val))}
-        else:
-            raise RuntimeError
-
-    return {k: list(map(unleaf_helper, v)) for k, v in tt.items()}
-
 # Grab filename from commandline.
 if len(sys.argv) < 2:
     print('usage: dumptree.py <nwbfile>', file=sys.stderr)
@@ -48,7 +27,6 @@ tree = data.tree.to_dict()
 
 # Normalize the tree into something more easily traversable.
 tree_norm = normalize(tree)
-tree_flat = unleaf(tree_norm)
 
 # Dump the normalized tree out as JSON.
-print(json.dumps(tree_flat, indent=2))
+print(json.dumps(tree_norm, indent=2))
